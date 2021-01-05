50302e8b40262d9ff1e261d578b1fdc439684eb5
Adam W. Willis <return.of.octobot@gmail.com>
2021-01-05 12:10:07 +0100

    treewide: Don't warn on debugfs failures when DEBUG_FS is disabled
    It stands to reason that if I disable DEBUG_FS, then dependant
    functions will fail. There is no need to redundantly alert to this
    in dmesg, unless these calls are failing when DEBUG_FS is actually
    enabled. Guard applicable emissions accordingly, where possible,
    otherwise demote to debug.
    
    Signed-off-by: Adam W. Willis <return.of.octobot@gmail.com>

--- techpack/video/msm/vidc/msm_vidc_debug.c
+++ techpack/video/msm/vidc/msm_vidc_debug.c
@@ -271,7 +271,7 @@ struct dentry *msm_vidc_debugfs_init_core(struct msm_vidc_core *core,
 	if (IS_ERR_OR_NULL(dir)) {
 		dir = NULL;
 #ifdef CONFIG_DEBUG_FS
-		d_vpr_e("Failed to create debugfs for msm_vidc\n");
+		pr_debug("Failed to create debugfs for msm_vidc\n");
 #endif
 		goto failed_create_dir;
 	}
@@ -494,7 +494,7 @@ struct dentry *msm_vidc_debugfs_init_inst(struct msm_vidc_inst *inst,
 	if (IS_ERR_OR_NULL(dir)) {
 		dir = NULL;
 #ifdef CONFIG_DEBUG_FS
-		s_vpr_e(inst->sid, "Failed to create debugfs for msm_vidc\n");
+		pr_debug(inst->sid, "Failed to create debugfs for msm_vidc\n");
 #endif
 		goto failed_create_dir;
 	}
